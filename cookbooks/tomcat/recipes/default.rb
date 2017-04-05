ed18a0eb78281ca2b799d02dc4f91f9bceff4dd0
vagrant <vagrant@localhost.localdomain>
2017-04-05 01:37:28 +0000

    jdk install and group creation

--- cookbooks/tomcat/recipes/default.rb
+++ cookbooks/tomcat/recipes/default.rb
@@ -3,3 +3,14 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+#
+
+# Install OpenJDK
+yum_package 'java-1.7.0-openjdk-devel'
+
+# Create user and group
+group 'chef'
+user 'chef' do
+  gid 'chef'
+end
+
