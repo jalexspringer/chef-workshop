791effd355f731301bee07389b52b4b1610cf3d0
Alex S <jalexspringer@gmail.com>
2017-04-05 03:19:04 +0000

    tomcat dled

--- cookbooks/tomcat/metadata.rb
+++ cookbooks/tomcat/metadata.rb
@@ -1,10 +1,10 @@
 name 'tomcat'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Alex Springer'
+maintainer_email 'jalexspringer@gmail.com'
 license 'all_rights'
 description 'Installs/Configures tomcat'
-long_description 'Installs/Configures tomcat'
-version '0.1.0'
+long_description 'Installs/Configures Tomcat according to the set of instructions given by the chef workshop.'
+version '0.2.1'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
--- cookbooks/tomcat/recipes/default.rb
+++ cookbooks/tomcat/recipes/default.rb
@@ -14,3 +14,8 @@ user 'chef' do
   gid 'chef'
 end
 
+# Download and extract Tomcat binary
+remote_file '/tmp/apache-tomcat-8.0.9.tar.gz' do
+  source 'https://archive.apache.org/dist/tomcat/tomcat-8/v8.0.9/bin/apache-tomcat-8.0.9.tar.gz'
+end
+
