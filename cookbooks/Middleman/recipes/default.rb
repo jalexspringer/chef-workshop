ab14dabc5a1a1700b104b9ab842810ae1b67a676
Alex S <jalexspringer@gmail.com>
2017-04-07 21:31:15 +0000

    Ruby issues overcome.

--- cookbooks/Middleman/recipes/default.rb
+++ cookbooks/Middleman/recipes/default.rb
@@ -10,27 +10,41 @@ apt_update 'periodic' do
   action :periodic
 end
 
+directory '/home/middleman'
+directory '/home/middleman/'
+
+user 'middleman' do
+  home '/home/middleman'
+end
+
+template '/etc/sudoers.d/middleman_conf' do
+  source 'middleman_conf.erb'
+end
+
 # Install required packages
 package [ 'build-essential', 'libssl-dev', 'libyaml-dev', 'libreadline-dev', 'openssl', 'curl', 'git-core', 'zlib1g-dev', 'bison', 'libxml2-dev', 'libxslt1-dev', 'libcurl4-openssl-dev', 'nodejs', 'libsqlite3-dev', 'sqlite3' ]
 
 # Download and install ruby from source
-remote_file '/tmp/ruby-2.1.3.tar.gz' do
-  source 'http://cache.ruby-lang.org/pub/ruby/2.1/ruby-2.1.3.tar.gz'
-  notifies :run, 'bash[install_ruby]', :immediately
-end
+#remote_file '/home/middleman/ruby/ruby-2.1.3.tar.gz' do
+#  source 'http://cache.ruby-lang.org/pub/ruby/2.1/ruby-2.1.3.tar.gz'
+#  notifies :run, 'bash[install_ruby]', :immediately
+#end
 
-bash 'install_ruby' do
-  user 'root'
-  cwd '/tmp'
-  code <<-EOH
-    tar -xzf ruby-2.1.3.tar.gz
-    (cd ruby-2.1.3/ && ./configure && make && make-install)
-  EOH
-  action :nothing
-end
+package ['ruby', 'ruby-dev']
+
+#bash 'install_ruby' do
+#  user 'middleman'
+#  cwd '/home/middleman/ruby'
+#  code <<-EOH
+#    tar -xzf ruby-2.1.3.tar.gz
+#    (cd ruby-2.1.3/ && ./configure && make && make-install)
+#  EOH
+#  action :nothing
+#end
+#
 
 bash 'ruby_core_commands' do
-  user 'root'
+  user 'middleman'
   cwd '/'
   code <<-EOH
     cp /usr/local/bin/ruby /usr/bin/ruby
@@ -65,22 +79,26 @@ end
 # Install git and clone repo
 package 'git'
 
-git 'middleman-blog' do
+git '/home/middleman/middleman-blog' do
   repository node[:middleman][:git_repository]
 end
   
+directory '/home/middleman/middleman-blog' do
+  owner 'middleman'
+end
+
+file '/home/middleman/middleman-blog/*' do
+  owner 'middleman'
+end
+
 # Install Bundler
 gem_package 'bundler'
 
-user 'middleman' do
-  comment 'User to install ruby bundle'
-  shell '/bin/bash'
-end
-
 bash 'bundle_install' do
   user 'middleman'
-  cwd '/middleman-blog'
+  cwd '/home/middleman/middleman-blog'
   code <<-EOH
-    sudo bundle install
+    bundle update
+    bundle install
   EOH
 end
